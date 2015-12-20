2370a80630a807e95a738a284334edf866c978be
Raymond <ray@firestitch.com>
2015-12-20 13:56:33 -0500

    .

---
+++ js/docs-setup.js
@@ -0,0 +1,24 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fs-angular-api.services:fsApi",
+      "shortName": "fsApi",
+      "type": "interface",
+      "moduleName": "fs-angular-api",
+      "keywords": "ajax api appened arguments base call callback code codes complete completing data datakey defaults delete encoding endpoint event events fail formdata fs-angular-api function http interface json key method milliseconds number object optional options override path post query range represents request response returns send services specifies start string success timeout timesout type url"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
