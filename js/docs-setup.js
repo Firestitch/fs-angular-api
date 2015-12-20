6dbcebee92c762216f23378b9083219931339050
Raymond <ray@firestitch.com>
2015-12-20 14:23:19 -0500

    .

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -9,7 +9,7 @@ NG_DOCS={
       "shortName": "fsApi",
       "type": "interface",
       "moduleName": "fs-angular-api",
-      "keywords": "ajax api appened arguments base call callback code codes complete completing data datakey defaults delete encoding endpoint event events fail formdata fs-angular-api function http interface json key method milliseconds number object optional options override path post query range represents request response returns send services specifies start string success timeout timesout type url"
+      "keywords": "ajax api appened apply arguments array base call callback code codes compelte complete completing data datakey defaults delete encoding endpoint event events fail formdata fs-angular-api function http interface json key method milliseconds number object optional options override parameter passed path post query range represents request response returns send services specifies start string success target targeted timeout timesout type url"
     }
   ],
   "apis": {
