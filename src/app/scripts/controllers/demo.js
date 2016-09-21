9e95b0e48ca5ce9b2430827ad51796e8e36a7180
Raymond <ray@firestitch.com>
2016-09-21 17:08:35 -0400

    removed double line comments and code lines added sanitize() that processes get and post data

--- src/app/scripts/controllers/demo.js
+++ src/app/scripts/controllers/demo.js
@@ -4,23 +4,23 @@
 angular.module('app')
 .controller('DemoCtrl', function ($scope, DummyService, fsApi) {
 
-	
-	fsApi.get('dummy', { count: 10, date: new Date() }, { headers: { 'Api-Key': '7234987sdtf8345rtgasyhd' }})
+
+	fsApi.get('dummy', { count: 10, date: new Date(), test: undefined }, { headers: { 'Api-Key': '7234987sdtf8345rtgasyhd' }})
 	.then(function(data) {
 		$scope.data = data;
 	});
 
-	fsApi.post('dummy', { count: 10, date: new Date() }, { headers: { 'Api-Key': '7234987sdtf8345rtgasyhd' }})
+	fsApi.post('dummy', { count: 10, date: new Date(), test111: 111 }, { headers: { 'Api-Key': '7234987sdtf8345rtgasyhd' }})
 	.then(function(data) {
-		
+
 	});
 
-	var data  = { count: 10, date: moment(), object: { date2: moment() } };
+/*	var data  = { count: 10, date: moment(), object: { date2: moment() } };
 
 	fsApi.post('dummy', data, { headers: { 'Api-Key': '7234987sdtf8345rtgasyhd' }})
 	.then(function(data) {
-		
-	});
+
+	});*/
 
 });
 
